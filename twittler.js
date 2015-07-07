5cb0efbab918db31c84729ec72338110b9068ab8
Matthew Brooks <mdeanbrooks@gmail.com>
2015-07-06 23:05:13 -0700

    Implement beta display of new tweets. Update tweet stream styling. Increase frequency of tweet generation.

--- twittler.js
+++ twittler.js
@@ -1,22 +1,34 @@
+var lastIndex = 0; 
+
+var addTweets = function() {
+  var $tweetStream = $('.tweet-stream');
+  var lastTweetDisplayed = streams.home.length - 1;
+  var index = streams.home.length - 1;
+
+  while(index >= lastIndex){
+    var tweet = streams.home[index];
+    var $tweetBox = $('<section></section>').addClass('tweet-box');
+    var $timeStamp = $('<span></span>').addClass('time-stamp');
+    var $tweetUser = $('<span></span>').addClass('user-name');
+    var $tweetMessage = $('<article></article>').addClass('tweet-message');
+    $tweetUser.text('@' + tweet.user).appendTo($tweetBox);
+    $timeStamp.text(moment().fromNow(tweet.created_at) + " ago.").appendTo($tweetBox);
+    $tweetMessage.text(tweet.message).appendTo($tweetBox);
+    $tweetBox.prependTo($tweetStream);
+    index -= 1;
+  }
+
+  lastIndex = lastTweetDisplayed;
+  console.log(lastIndex);
+};
+
 $(document).ready(function(){
-        var $tweetStream = $('.tweet-stream');
 
-        var index = streams.home.length - 1;
-        while(index >= 0){
-          
-          var tweet = streams.home[index];
-          var $tweetBox = $('<section></section>').addClass('tweet-box');
-          var $timeStamp = $('<span></span>').addClass('time-stamp');
-          var $tweetUser = $('<span></span>').addClass('user-name');
-          var $tweetMessage = $('<article></article>').addClass('tweet-message');
-          
-          $tweetUser.text('@' + tweet.user).appendTo($tweetBox);
-          $timeStamp.text(moment().fromNow(tweet.created_at)).appendTo($tweetBox);
-          $tweetMessage.text(tweet.message).appendTo($tweetBox);
+  addTweets();
 
-          $tweetBox.appendTo($tweetStream);
-          
-          index -= 1;
-        }
+});
 
-      });
+setInterval(addTweets, 6000);
+
+
+
