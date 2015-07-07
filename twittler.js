4a13782109a3dc21508be0b25c1fe3db21f6f1a3
Matthew Brooks <mdeanbrooks@gmail.com>
2015-07-06 21:11:16 -0700

    Use jQuery to create html elements for each tweet.

--- twittler.js
+++ twittler.js
@@ -4,9 +4,17 @@ $(document).ready(function(){
         var index = streams.home.length - 1;
         while(index >= 0){
           var tweet = streams.home[index];
-          var $tweet = $('<div></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message);
-          $tweet.appendTo($tweetStream);
+          var $tweetBox = $('<section></section>').addClass('tweet-box');
+          var $timeStamp = $('<span></span>').addClass('time-stamp');
+          var $tweetUser = $('<span></span>').addClass('user-name');
+          var $tweetMessage = $('<article></article>').addClass('tweet-message');
+          
+          $tweetUser.text('@' + tweet.user).appendTo($tweetBox);
+          $timeStamp.text(moment().fromNow(tweet.created_at)).appendTo($tweetBox);
+          $tweetMessage.text(tweet.message).appendTo($tweetBox);
+
+          $tweetBox.appendTo($tweetStream);
+          
           index -= 1;
         }
 
