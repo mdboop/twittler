f3f2ab90a89c5865dca9fc14d998f6c04cf6bdf2
Matthew Brooks <mdeanbrooks@gmail.com>
2015-07-07 12:07:33 -0700

    Implement View Tweets button and change addTweets to use for loop.

--- twittler.js
+++ twittler.js
@@ -2,33 +2,28 @@ var lastIndex = 0;
 
 var addTweets = function() {
   var $tweetStream = $('.tweet-stream');
-  var lastTweetDisplayed = streams.home.length - 1;
-  var index = streams.home.length - 1;
-
-  while(index >= lastIndex){
-    var tweet = streams.home[index];
+  for (var i = lastIndex; i < streams.home.length; i+= 1) {
+    var tweet = streams.home[i];
     var $tweetBox = $('<section></section>').addClass('tweet-box');
     var $timeStamp = $('<span></span>').addClass('time-stamp');
     var $tweetUser = $('<span></span>').addClass('user-name');
     var $tweetMessage = $('<article></article>').addClass('tweet-message');
     $tweetUser.text('@' + tweet.user).appendTo($tweetBox);
-    $timeStamp.text(moment().fromNow(tweet.created_at) + " ago.").appendTo($tweetBox);
+    $timeStamp.text(moment(tweet.created_at).format("dddd, MMMM Do YYYY, h:mm:ss a")).appendTo($tweetBox);
     $tweetMessage.text(tweet.message).appendTo($tweetBox);
     $tweetBox.prependTo($tweetStream);
-    index -= 1;
+    lastIndex = i + 1;
   }
-
-  lastIndex = lastTweetDisplayed;
-  console.log(lastIndex);
 };
 
 $(document).ready(function(){
 
   addTweets();
 
+  $('button').click(addTweets);
+
 });
 
-setInterval(addTweets, 6000);
 
 
 
